9009dcc25a7b6965546581414821a2d8c331ab66
Gemma Shearer <gemmaandjude@gmail.com>
2015-06-10 12:27:44 +0100

    set up outlets and actions

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //connections for the lables so that these can be changed dynamically
+    
+    @IBOutlet weak var youHaveMoneyLabel: UILabel!
+    @IBOutlet weak var youHaveLemonsLabel: UILabel!
+    @IBOutlet weak var youHaveIceCubesLabel: UILabel!
+    @IBOutlet weak var purchaseLemonsLabel: UILabel!
+    @IBOutlet weak var purchaseIceCubesLabel: UILabel!
+    @IBOutlet weak var mixLemonsLabel: UILabel!
+    @IBOutlet weak var mixIceCubesLabel: UILabel!
+    
+    //Connections for buttons are under didReceiveMemoryWarning
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buyMoreLemonsPressed(sender: UIButton) {
+    }
+    
+    @IBAction func buyFewerLemonsPressed(sender: UIButton) {
+    }
+    
+    @IBAction func buyMoreIcePressed(sender: UIButton) {
+    }
 
+    @IBAction func buyLessIcePressed(sender: UIButton) {
+    }
+    
+    
+    @IBAction func mixMoreLemonsPressed(sender: UIButton) {
+    }
+    
+    @IBAction func mixFewerLemonsPressed(sender: UIButton) {
+    }
+    
+    @IBAction func mixMoreIcePressed(sender: UIButton) {
+    }
+    
+    @IBAction func mixLessIcePressed(sender: UIButton) {
+    }
+    
+    
+    @IBAction func startButtonPressed(sender: UIButton) {
+    }
+    
 }
 
